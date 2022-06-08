fef0b1267d5bc3926f1465cf24944f8660e6a777
Girolamo <girolamosciascia@gmail.com>
2022-06-08 12:23:29 +0200

    aggiunto file java

---
+++ Main2.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		Rettangolo rettangolo = new Rettangolo(5, 8);
+		System.out.println("Il perimetro del rettangolo:" + rettangolo.calcolaPerimetro());
+		
+		System.out.println("Il area del rettangolo:" + rettangolo.calcolaArea());
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
